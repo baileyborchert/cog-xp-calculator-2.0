759da8e0a78e6828808576f0275373d31172330b
Bailey Borchert <baileyborchert@gmail.com>
2024-07-23 22:21:23 -0500

    BUILD: Built UI toggle to switch active Cog type

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
+  <ul>
+    <li
+      v-for="cogType of cogTypes"
+      :key="cogType"
+    >
+      <button
+        type="button"
+        v-text="cogType"
+        @click="switchActiveCogType(cogType)"
+      />
+    </li>
+  </ul>
+
   <CogHeadquarters
+    v-if="activeCogType === 'Sellbot'"
     cog-type="Sellbot"
     xp-name="Merit"
   />
+
   <CogHeadquarters
+    v-if="activeCogType === 'Cashbot'"
     cog-type="Cashbot"
     xp-name="Cogbuck"
   />
+
   <CogHeadquarters
+    v-if="activeCogType === 'Lawbot'"
     cog-type="Lawbot"
     xp-name="Jury Notice"
   />
+
   <CogHeadquarters
+    v-if="activeCogType === 'Bossbot'"
     cog-type="Bossbot"
     xp-name="Stock Option"
   />
@@ -22,8 +42,26 @@ import CogHeadquarters from './components/CogHeadquarters.vue'
 
 export default {
   name: 'App',
+
   components: {
     CogHeadquarters
+  },
+
+  data() {
+    return {
+      activeCogType: 'Sellbot',
+      cogTypes: ['Sellbot', 'Cashbot', 'Lawbot', 'Bossbot'],
+    }
+  },
+
+  methods: {
+    /**
+     * Switches active Cog type.
+     * @param {String} cogType 
+     */
+    switchActiveCogType(cogType) {
+      this.activeCogType = cogType
+    },
   }
 }
 </script>
